e0b568b341edd97941ecc3effa54ca1fa3fdcc85
DevendiranKathir <devendirank61@gmail.com>
2026-01-20 18:22:15 +0530

    day 2: setup sqlite database and user model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from .database import engine
+from . import models
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
     return {"Hello": "This is From Backend"} 
+
